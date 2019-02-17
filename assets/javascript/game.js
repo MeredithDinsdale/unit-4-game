76740e993870e7316dcd4afe3767c0613091e85f
Meredith <dinsdale.meredith@gmail.com>
2019-02-16 21:11:31 -0800

    completed hw assignment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,170 @@
 
-//global variables-----------------------------------------------------
-
 
 let targetNumber = Math.floor(Math.random() * (121 - 19)) + 19;
-console.log('Target Number: ' + targetNumber);
+
+let beachDeathArray = ['a surprise tsunami.', 'a giant snapping sea dragon attack.', 'a brutal crab army.',  'an electric eel.', 'being struck by lightning.', 'one thousand jellyfish stings.', 'being mauled by a sealion.', 'a dolphin pulling you underwater and keeping you there.', 'scavenging wild dogs.', 'an attack by sea birds.'];
+let jungleDeathArray = ['an attack by a tiger.', 'quicksand.', 'being strangled by vines.', 'being swallowed by a snake.', 'a bit from a giant poisonous spider.', 'being murdered by monkeys.', 'falling into a pit lined with spikes.', 'being absorbed by the jungle. You are now fossilized as a part of the trees, for all eternity.', 'one million bee stings.', 'being consumed alive by wasps.'];
+let caveDeathArray = ['a mysterious drowning.', 'being impaled on a stalagmite.', 'being devoured by a cave fish.', 'vanishing into the inky darkness.', 'an attack by giant bats.', 'dissolution in a caustic slime.', 'being crushed in a cave collapse.', 'getting inexplicably lost and starving to death.', 'suffocation when your entry path caves in.'];
+
+let wins = 0;
+let losses = 0;
+let pointCounter = 0;
+
+let specimen1 = 0;
+let specimen2 = 0;
+let specimen3 = 0;
+let specimen4 = 0;
+
+let image1 = Math.floor(Math.random() * (13 - 2)) + 2;
+let image2 = Math.floor(Math.random() * (13 - 2)) + 2;
+let image3 = Math.floor(Math.random() * (13 - 2)) + 2;
+let image4 = Math.floor(Math.random() * (13 - 2)) + 2;
+
+let path = window.location.pathname;
+
 $("#number-to-guess").text(targetNumber);
 
-let wins = losses = points = 0
-let pointOptions = Math.floor(Math.random() * (13 - 1)) + 1;
+$('#specimen1').text(specimen1);
+$('#specimen2').text(specimen2);
+$('#specimen3').text(specimen3);
+$('#specimen4').text(specimen4);
 
-for (var i = 0; i < numberOptions.length; i++) {
-    let imageCrystal = $("<img>");
-    imageCrystal.addClass("specimens");
-    imageCrystal.attr("src", "");
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-    $(".crystal-image").on("click", function() {
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
-        points += crystalValue;
-        document.getElementById("points").innerHTML = points;
+$('#wins').text(wins);
+$('#losses').text(losses);
+$('#points').text(pointCounter);
 
-// var audioElement = document.createElement("audio");
-//     audioElement.setAttribute("src", "assets/javascript/Twinkle-sound-effect.mp3");
+console.log('Target Number: ' + targetNumber);
+console.log('Image1 is worth ' + image1 + ' points. Image2 is worth: ' + image2 + ' points. Image3 is worth ' + image3 + ' points. Image4 is worth ' + image4 + ' points.');
+console.log(window.location.pathname);
+console.log('Path: ' + path);
 
 $(document).ready(function() {
-//onKey event triggerred when user presses letter key ---------------------------------------------------------------------
 
-    document.onkeyup=function(event) {
-        var userGuess = event.key.toUpperCase();
-        console.log('User guess: ' + userGuess);
-        if (Alphanumeric(event.key)){ 
-            document.getElementById("message").innerHTML ="";
-            crystalBall.animate({ opacity: "0" }, 1000);
-//If user wins-------------------------------------------------------------------------------------------------------------      
-        if (points === targetNumber) {
-            wins +=1;
-            console.log('Number of wins: ' + wins);
-            document.getElementById("message").innerHTML ="<h5>You win!</h5>";
-            points = 0;
-            targetNumber = [Math.floor(Math.random() * 120)];
-            console.log('Target Number: ' + targetNumber);
-            }
-//If points go over the target number----------------------------------------------------------------------------------------------------
-            
-        else if (points > targetNumber) {
-            document.getElementById("message").innerHTML ="<h5>You Lost</h5>";
-            losses +=1;
-            points = 0;
-            targetNumber = [Math.floor(Math.random() * 120)];
-            console.log('Target Number: ' + targetNumber);
-            console.log('Number of losses: ' + losses);
-        }
+
+  let beachAudioElement = document.createElement("audio");
+      beachAudioElement.setAttribute("src", "assets/beachSound.mp3");
+      $("#beachButton").on("click", function() {
+        beachAudioElement.play();
+      });
+
+  let jungleAudioElement = document.createElement("audio");
+      jungleAudioElement.setAttribute("src", "assets/jungleSound.mp3");
+      $("#jungleButton").on("click", function() {
+          jungleAudioElement.play();
+   });
+
+  let caveAudioElement = document.createElement("audio");
+      caveAudioElement.setAttribute("src", "assets/caveSound.mp3");
+      $("#caveButton").on("click", function() {
+          caveAudioElement.play();
+   });
+
+
+ 
+  $("#image1").on("click", function() {
+    pointCounter = pointCounter + image1;
+    console.log('points: ' + pointCounter);
+    $('#points').text(pointCounter);
+    specimen1++;
+    $('#specimen1').text(specimen1);
+    if (pointCounter === targetNumber) {
+      winner();
+      reset();}
+      else if (pointCounter > targetNumber) {
+        loser();
+        reset();
     }
-document.getElementById("win").innerHTML = 'Wins: ' + wins;
-document.getElementById("loss").innerHTML = 'Losses: ' + losses;
-}
+  });
+
+  $("#image2").on("click", function() {
+    pointCounter = pointCounter + image2;
+    console.log('points: ' + pointCounter);
+    $('#points').text(pointCounter);
+    specimen2++;
+    $('#specimen2').text(specimen2);
+    if (pointCounter === targetNumber) {
+      winner();
+      reset();}
+      else if (pointCounter > targetNumber) {
+        loser();
+        reset();
+    }
+  });
+
+  $("#image3").on("click", function() {
+    pointCounter = pointCounter + image3;
+    console.log('points: ' + pointCounter);
+    $('#points').text(pointCounter);
+    specimen3++;
+    $('#specimen3').text(specimen3);
+    if (pointCounter === targetNumber) {
+      winner();
+      reset();}
+      else if (pointCounter > targetNumber) {
+        loser();
+        reset();
+    }
+  });
+
+  $("#image4").on("click", function() {
+    pointCounter = pointCounter + image4;
+    console.log('points: ' + pointCounter);
+    $('#points').text(pointCounter);
+    specimen4++;
+    $('#specimen4').text(specimen4);
+    if (pointCounter === targetNumber) {
+      winner();
+      reset();}
+      else if (pointCounter > targetNumber) {
+        loser();
+        reset();
+    }
+  });
+
   
+ function winner() {
+  alert('You\'ve made it out with your collection and your life intact, congratulations! You can try to collect more specimens here, or scroll to the top for links to the other parts of the island.');
+  console.log('You Won!');
+  wins++;
+ }
 
+ function loser() {
+  console.log('You Lost...');
+  losses++;
+  if (path === '/C:/Users/Meredith/Documents/code/unit-4-game/beach.html') {
+    let beachDeath = beachDeathArray[Math.floor(Math.random() * beachDeathArray.length)];
+    alert('You have been killed by ' +beachDeath);
+  }
+  else if (path === '/C:/Users/Meredith/Documents/code/unit-4-game/jungle.html') {
+    let jungleDeath = jungleDeathArray[Math.floor(Math.random() * jungleDeathArray.length)];
+    alert('You have been killed by ' +jungleDeath);
+  }
+  else if (path === '/C:/Users/Meredith/Documents/code/unit-4-game/cave.html') {
+    let caveDeath = caveDeathArray[Math.floor(Math.random() * caveDeathArray.length)];
+    alert('You have been killed by ' +caveDeath);
+  }
+  }
 
-//functions---------------------------------------------------------
-  
-   
-});   
-
-
-
+  function reset() {
+    pointCounter = 0;
+    specimen1 = 0;
+    specimen2 = 0;
+    specimen3 = 0;
+    specimen4 = 0;
+    targetNumber = Math.floor(Math.random() * (121 - 19)) + 19;
+    image1 = Math.floor(Math.random() * (13 - 2)) + 2;
+    image2 = Math.floor(Math.random() * (13 - 2)) + 2;
+    image3 = Math.floor(Math.random() * (13 - 2)) + 2;
+    image4 = Math.floor(Math.random() * (13 - 2)) + 2;
+    $('#specimen1').text(specimen1);
+    $('#specimen2').text(specimen2);
+    $('#specimen3').text(specimen3);
+    $('#specimen4').text(specimen4);
+    $("#number-to-guess").text(targetNumber);
+    $('#points').text(pointCounter);
+    $('#losses').text(losses);
+    $('#wins').text(wins);
+    console.log('Target Number: ' + targetNumber);
+    console.log('Image1 is worth ' + image1 + ' points. Image2 is worth: ' + image2 + ' points. Image3 is worth ' + image3 + ' points. Image4 is worth ' + image4 + ' points.');
+  }
+});
